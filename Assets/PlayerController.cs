4f4530d1e720f735734e92a3e33ac12e4cccb18c
Federico Mercurio <fefe.mercurio@gmail.com>
2020-07-13 17:49:20 +0200

    InputManager del 13/7/2020

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("SPARO!");
+        }
+
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        transform.Translate(0f, 0f, vertical * .1f);
+        transform.Rotate(Vector3.up, horizontal);
+
+        Vector3 velocity = new Vector3(0f, 0f, 1f);
+    }
+}
